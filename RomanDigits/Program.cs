a61addd421b451b8ca6612064d07a6901fd29306
PlasticHead21 <PlasticHead23@gmail.com>
2018-05-06 16:03:58 +0300

    Parsing a roman numerals into arabic numerals

---
+++ RomanDigits/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+            string num = "";
+            string result = "";
+            int tmp = 0;
+            num = Console.ReadLine();
+            num = num.ToUpper();
+            var charArr = num.ToCharArray();
+            for(int i = 0; i < charArr.Length; i++)
+            {
+                if(i < charArr.Length - 1)
+                {
+                    if(dict[charArr[i]] < dict[charArr[i + 1]])
+                    {
+                        tmp += dict[charArr[i + 1]] - dict[charArr[i]];
+                        i++;
+                        continue;
+                    }
+                }
+                tmp += dict[charArr[i]];               
+            }
+            result = tmp.ToString();
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ RomanDigits/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RomanDigits")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RomanDigits")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("017eb2e2-9089-4ff4-baea-5730d60a51f9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

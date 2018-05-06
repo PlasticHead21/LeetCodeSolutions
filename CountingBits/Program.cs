3fd67574aabc615ac54648e68bbdce8ee8853837
PlasticHead21 <PlasticHead23@gmail.com>
2018-05-06 15:59:29 +0300

    Counting Bits in number
    Counting bits(1) in number.

---
+++ CountingBits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingDits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 245; //число
+            int count = 0; //количество бит равных 1
+            for (int i=0; i<8; i++)
+            {
+                if ((b >> i)%2 == 1)
+                    count++;
+            }
+            Console.WriteLine(count);
+            
+        }
+    }
+}
